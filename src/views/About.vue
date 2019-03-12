ab999eb57ffbee79943364b42e836af0cfb7f1e0
Jack Ryan <jack.dixon.ryan@gmail.com>
2019-03-12 08:54:08 -0600

    Foundational trivia game in one hour. Hard part involves syncing across devices and timing out each question.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Trivia Time!</h1>
     <div v-if="trivia">
-      <div v-for="question in trivia">
+      <div v-for="(question, questionIndex) in trivia" :key="question.question">
         <h3>{{question.question}}</h3>
+        <div v-for="answer in randomizedAnswers(question)" :key="answer">
+          <input type="radio" :name="answer" :id="answer" v-model="answers[questionIndex]" :value="answer">
+          <label :for="answer">{{answer}}</label>
+        </div>
       </div>
     </div>
+    <button @click="submitAnswers">Check Answers</button>
   </div>
 </template>
 
@@ -15,25 +20,57 @@ import Axios from 'axios';
 export default {
   data() {
     return {
-      trivia: null
+      trivia: null,
+      answers: [],
     }
   },
   computed: {
  
   },
   beforeMount() {
+    // get the API
     Axios.get('https://opentdb.com/api.php?amount=10&category=9&difficulty=hard')
       .then(HTTPresponse => {
         const questionsArray = HTTPresponse.data.results;
+        // iterate through the questions
         for (let i = 0; i < questionsArray.length; i++) {
+          // take out all html codes and sub them with actual characters
           const withApostrophes = questionsArray[i].question.split("&#039;").join("'");
           const withQuotations = withApostrophes.split('&quot;').join('"');
-          console.log(withQuotations);
+
+          // then set the question equal to the newly legible form.
           questionsArray[i].question = withQuotations;
         }
+        // and set the data.
         this.trivia = questionsArray;
+        this.answers.length = questionsArray.length;
       })
       .catch(err => console.log());
+  },
+  methods: {
+    randomizedAnswers(question) {
+      const returnArray = [];
+      returnArray.push(question.correct_answer)
+      question.incorrect_answers.map(answer => returnArray.push(answer));
+      return returnArray;
+    },
+    submitAnswers() {
+      const correct = [];
+      const incorrect = [];
+      for (let i = 0; i < this.trivia.length; i++) {
+        if (this.trivia[i].correct_answer === this.answers[i]) {
+          correct.push(this.trivia[i]);
+        } else {
+          incorrect.push({
+            question: this.trivia[i],
+            userAnswer: this.answers[i],
+            correctAnswer: this.trivia[i].correct_answer
+          });
+        }
+      }
+      console.log(correct.length);
+      console.log(incorrect);
+    }
   }
 }
 </script>
